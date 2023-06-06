b79d1db9308ae6d1e46d32f296389b53135bf26c
Metaflax <mstf.cobann1@gmail.com>
2023-06-06 16:39:38 +0300

    Select Box - End of Project

--- second-react-app/src/components/NewEventForm.js
+++ second-react-app/src/components/NewEventForm.js
@@ -1,26 +1,27 @@
 import './NewEventForm.css'
-import {useState, useRef} from 'react'
+import {useState} from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 export default function NewEventForm({ addEvent }) {
-    // const [title, setTitle] = useState ('')
-    // const [date, setDate] = useState ('')
-    const title = useRef()
-    const date = useRef()
+    const [title, setTitle] = useState ('')
+    const [date, setDate] = useState ('')
+    const [location, setLocation] = useState('ankara')
     
     const resetForm = () => {
-        title.current.value = ""
-        date.current.value = ""
+        setTitle('')
+        setDate('')
+        setLocation('ankara')
     }
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(title,date)
 
         const event = {
-            title: title.current.value,
-            date: date.current.value,
+            title: title,
+            date: date,
+            location: location,
             id: uuidv4()
         }
+        console.log(event)
         addEvent(event)
         resetForm()
     }
@@ -30,16 +31,26 @@ export default function NewEventForm({ addEvent }) {
         <label>
             <span>Event Title:</span>
             <input 
-                type="text"
-                ref={title}
-            />
+                type="text" 
+                onChange={(e) => setTitle(e.target.value)} 
+                value={title}>
+            </input>
         </label>
         <label>
             <span>Event Date:</span>
             <input 
-                type="date"
-                ref={date}            
-            />
+                type="date" 
+                onChange={(e) => setDate(e.target.value)} 
+                value={date}>                
+            </input>
+        </label>
+        <label>
+            <span>Event location:</span>
+            <select onChange={(e) => setLocation(e.target.value)}>
+                <option value="ankara">Ankara</option>
+                <option value="izmir">İzmir</option>
+                <option value="istanbul">İstanbul</option>
+            </select>
         </label>
         <button>Submit</button>
     </form>
